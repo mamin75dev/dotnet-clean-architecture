5fc1e33265542a64ac57a7815b2186296db063f3
mohamadamin <mamin75.dev@gmail.com>
2023-12-30 18:16:41 +0330

    first

---
+++ Contracts/AccountForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts
+{
+    public class AccountForCreationDto
+    {
+        public DateTime DateCreated { get; set; }
+
+        public string AccountType { get; set; }
+    }
+}
